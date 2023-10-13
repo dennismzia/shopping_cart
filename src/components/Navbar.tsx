76abb7215413c7e1aed7f46195571771711bb905
Dennis malware <davspencer30@gmail.com>
2023-10-13 06:55:27 +0300

    updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import {Navbar as NavbarBs, Container} from "react-bootstrap";
+import {Navbar as NavbarBs, Container,Nav} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <NavbarBs className="bg-white shadow-sm mb-3">
         <Container>
-            Nav
+            <Nav>
+                <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+                <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
+                <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+            </Nav>
         </Container>
     </NavbarBs>
 
