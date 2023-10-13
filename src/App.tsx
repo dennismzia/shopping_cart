688575c9c26f4ae4a5955c37a1fe5e64c797c9e5
Dennis malware <davspencer30@gmail.com>
2023-10-13 06:24:33 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+import { About } from './pages/About';
 
 const App = () => {
   return (
-    <Container>      Hi
-
+    <Container> 
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/store" element={<Store/>} />
