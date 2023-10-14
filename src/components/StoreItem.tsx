63a537c54c77c360cb408d480920d98a6e275e7d
Dennis malware <davspencer30@gmail.com>
2023-10-14 12:01:20 +0300

    updates

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
-
-export const StoreItem = () => {
+type StoreItemProps = {
+    id: number,
+    name: string,
+    price: number,
+    imgUrl: string
+}
+export const StoreItem = ({id, name , price, imgUrl}: StoreItemProps) => {
   return (
     <div>StoreItem</div>
   )
