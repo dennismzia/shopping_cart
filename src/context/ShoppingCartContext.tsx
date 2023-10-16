fd8dbc2f0bcaca72bd540d36539c4089f26033e7
Dennis malware <davspencer30@gmail.com>
2023-10-16 08:10:13 +0300

    added some cart functionality

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -34,9 +34,43 @@ export const ShoppingCartProvider = ({children}:shoppingCartProviderProps) => {
 
     }
 
+    const increaseCartQuantity = (id: number) => {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id) == null){
+                return [...currItems,{id,quantity:1}]
+            }else {
+                return currItems.map(item => {
+                    if (item.id === id){
+                        return {...item,quantity: item.quantity + 1}
+                    }else{
+                        return item
+                    }
+                })
+            }
+        })
+    }
+
+    const decreaseCartQuantity = (id: number) => {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id)?.quantity === 1){
+                return currItems.filter(item => item.id !== id)
+            }else {
+                return currItems.map(item => {
+                    if (item.id === id){
+                        return {...item,quantity: item.quantity - 1}
+                    }else{
+                        return item
+                    }
+                })
+            }
+        })
+    }
+
     return(
         <shoppingCartContext.Provider value={{
-            getItemQuantity
+            getItemQuantity,
+            increaseCartQuantity,
+            decreaseCartQuantity
         }}>
             {children}
         </shoppingCartContext.Provider>
